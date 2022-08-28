ac1917813a0a2d04955e16cbd349dadd66c1748d
PavithranBaskaran <vktpavi@gmail.com>
2022-08-28 09:23:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
+import Heading from "./Component/Heading";
+import {Loader}  from "./Component/Loader";
+import { config } from "./config";
 
 function App() {
+  const [images, setImages] = useState([]);
+  // const {REACT_APP_ACCESSKEY} = process.env
+  const key = `nq-QaRTe3qTE1niKnRAS_U_9n6SFcCn1h_THHoxyts4`
+
+  const fetchImages = () => {
+    axios
+      .get(`${config.api}/photos/random?client_id=${key}&h=1300&dpr=2&orientation=portrait&count=5`)
+      .then((result) => {
+        setImages([...images, ...result.data]);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          if (error.response.status) {
+            alert(
+              `${error.response.status} : ${error.response.data}, try after an hour`
+            );
+          }
+        }
+      });
+  };
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Heading />
+      <div className="container-fluid ">
+        <InfiniteScroll
+          dataLength={images.length}
+          next={fetchImages}
+          hasMore={true}
+          loader={<Loader />}
+          className="row row-cols-lg-3"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {images.map((image) => {
+            return (
+              <>
+                <div className="container-image">
+                  <img
+                    src={`${image.urls.regular}`}
+                    className="img-fluid col mt-5 image"
+                    alt="..."
+                  ></img>
+                  <div class="middle">
+                    <div
+                      class="text"
+                      onClick={() => {
+                        window.open(`${image.urls.regular}`, "_blank");
+                      }}
+                    >
+                      View
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </InfiniteScroll>
+      </div>
+    </>
   );
 }
 
---
+++ src/Component/Heading.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import ContactEmergencyIcon from "@mui/icons-material/ContactEmergency";
+
+function Heading() {
+  return (
+    <>
+      <div className="container d-flex justify-content-between mt-4">
+        <div className="left fs-1 fw-normal">Pavithran</div>
+        <div className=" mt-lg-4 mt-md-5 d-flex justify-content-between ">
+          <LinkedInIcon
+            className="fs-2 me-4 right linkedin"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/pavithran-b-8026071a3/",
+                "_blank"
+              )
+            }
+          />
+          <GitHubIcon
+            className="fs-2 me-4 right github"
+            onClick={() =>
+              window.open("https://github.com/PavithranBaskaran", "_blank")
+            }
+          />
+          <ContactEmergencyIcon
+            className="fs-2 right profile"
+            onClick={() =>
+              window.open("https://pavithran-portfolio.netlify.app/", "_blank")
+            }
+          />
+        </div>
+      </div>
+      <div className="container mt-5 d-flex justify-content-center">
+        <h3 className="Title fw-normal">Unsplash Infinite Scroll</h3>
+      </div>
+    </>
+  );
+}
+
+export default Heading;
